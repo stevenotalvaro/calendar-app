04d1901d5429591b141374c249c5446347729d63
Steven Otalvaro <stevenotalvaroz@gmail.com>
2021-08-29 17:28:35 -0500

    Routers, Login and Register, navbar implemented

---
+++ src/ui/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <div className="navbar navbar-dark bg-dark mb-4">
+            <span className="navbar-brand">Miguelito</span>
+            <button className="btn btn-outline-danger">
+                <span>Salir</span>
+            </button>
+        </div>
+    )
+}
